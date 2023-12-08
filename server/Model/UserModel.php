507506dd257e554b61e85572252e0698edea4a67
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-12-08 15:04:19 +0100

    Add php server

---
+++ server/Model/UserModel.php
@@ -0,0 +1,12 @@
+<?php
+require_once PROJECT_ROOT_PATH . "/Model/Database.php";
+
+class UserModel extends Database
+{
+    public function getUsers($limit) 
+    {
+        return $this->select("SELECT * FROM customers ORDER BY customerNumber ASC LIMIT ?", ["i", $limit]);
+
+    }
+}
+?>
--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Function to fetch data from your localhost API
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/user/list');
+        if (response.ok) {
+          const result = await response.json();
+          setData(result);
+        } else {
+          throw new Error('Failed to fetch data');
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        // Handle error state or show an error message
+      }
+    };
+    // Call the function when the component mounts
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +41,13 @@ function App() {
         >
           Learn React
         </a>
+
+        {data && (
+          <div>
+            <p>Fetched Data:</p>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </div>
+        )}
       </header>
     </div>
   );
